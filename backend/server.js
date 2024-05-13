7f8753170684acf473315c41dad5b86a97d4b08b
whai2 <whai2125@gmail.com>
2024-05-13 17:02:13 +0900

    feat : 포트 연결과 모듈.js 로 변경

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from  "dotenv";
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-app.listen(4000, () => console.log("server Running..."));
+app.listen(4000, () => console.log(`server Running on ${PORT}` ));
