5f1907e4da74423cce17b3c70b32bf00f9a6dade
whai2 <whai2125@gmail.com>
2024-05-14 20:42:29 +0900

    feat : 회원가입, 로그인 기능 백엔드 구현, postman mock api를 통해 확인

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from  "dotenv";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+app.use(express.json());
 
-app.listen(4000, () => console.log(`server Running on ${PORT}` ));
+//middleware -> "api/auth/${authRoutes의 경로가 추가: signup, login, logout}"
+app.use("/api/auth", authRoutes);
+
+app.listen(4000, () => {
+  connectToMongoDB();
+  console.log(`server Running on ${PORT}` );
+});
