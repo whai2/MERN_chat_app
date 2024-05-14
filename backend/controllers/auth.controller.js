605d77f1e3da09d81e5b4d1a917a9d56b5b235fb
whai2 <whai2125@gmail.com>
2024-05-14 20:44:18 +0900

    feat : 로그아웃 기능 구현

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,6 +78,12 @@ export const login = async (req, res) => {
 	}
 };
 
-export const logout = async (req, res) => {
-  res.send("logoutUser");
-}
+export const logout = (req, res) => {
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
