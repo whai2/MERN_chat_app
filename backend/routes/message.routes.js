dd4b06f3cc297ff8286bed7d3bd186f01940fa5a
whai2 <whai2125@gmail.com>
2024-05-15 16:56:04 +0900

    feat: 대화방에 맞는 대화 메세지 get 호출

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-// router.get("/:id", protectRoute, getMessages);
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage); // protectRoute에 의해 
 
 export default router;
