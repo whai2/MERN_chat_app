1ae00ea8f7778d481dcb233bbb27a232954b6177
whai2 <whai2125@gmail.com>
2024-05-17 14:11:10 +0900

    feat: 로그인 상태 context api를 이용해, 전역 상태로 관리

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
