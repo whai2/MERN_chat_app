9130028443d96dd3c2f01c4220b88d9103a9ccc4
whai2 <whai2125@gmail.com>
2024-05-17 13:59:43 +0900

    feat: 회원 가입 기능 연결 구현

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
@@ -47,14 +47,15 @@ const Login = () => {
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
-					<a href='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
 
 					<div>
-						{/* <button className='btn btn-block btn-sm mt-2' disabled={loading}>
-							{loading ? <span className='loading loading-spinner '></span> : "Login"}
-						</button> */}
+						<button className='btn btn-block btn-sm mt-2'>
+							{/* {loading ? <span className='loading loading-spinner '></span> : "Login"} */}
+							Login
+						</button>
 					</div>
 				</form>
 			</div>
@@ -62,3 +63,46 @@ const Login = () => {
 	);
 };
 export default Login;
+
+
+// STARTER CODE FOR THIS FILE
+// const Login = () => {
+// 	return (
+// 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+// 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+// 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+// 					Login
+// 					<span className='text-blue-500'> ChatApp</span>
+// 				</h1>
+
+// 				<form>
+// 					<div>
+// 						<label className='label p-2'>
+// 							<span className='text-base label-text'>Username</span>
+// 						</label>
+// 						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+// 					</div>
+
+// 					<div>
+// 						<label className='label'>
+// 							<span className='text-base label-text'>Password</span>
+// 						</label>
+// 						<input
+// 							type='password'
+// 							placeholder='Enter Password'
+// 							className='w-full input input-bordered h-10'
+// 						/>
+// 					</div>
+// 					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+// 						{"Don't"} have an account?
+// 					</a>
+
+// 					<div>
+// 						<button className='btn btn-block btn-sm mt-2'>Login</button>
+// 					</div>
+// 				</form>
+// 			</div>
+// 		</div>
+// 	);
+// };
+// export default Login;
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,8 @@
-// import { Link } from "react-router-dom";
-// import GenderCheckbox from "./GenderCheckbox";
+import { Link } from "react-router-dom";
 import { useState } from "react";
-// import useSignup from "../../hooks/useSignup";
+
+import GenderCheckbox from "./GenderCheckBox";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
 	const [inputs, setInputs] = useState({
@@ -12,7 +13,7 @@ const SignUp = () => {
 		gender: "",
 	});
 
-	// const { loading, signup } = useSignup();
+	const { loading, signup } = useSignup();
 
 	const handleCheckboxChange = (gender) => {
 		setInputs({ ...inputs, gender });
@@ -20,7 +21,7 @@ const SignUp = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		// await signup(inputs);
+		await signup(inputs);
 	};
 
 	return (
@@ -83,20 +84,20 @@ const SignUp = () => {
 						/>
 					</div>
 
-					{/* <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} /> */}
+					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
-					<a
+					<Link
 						to={"/login"}
 						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
 						href='#'
 					>
 						Already have an account?
-					</a>
+					</Link>
 
 					<div>
-						{/* <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
 							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
-						</button> */}
+						</button>
 					</div>
 				</form>
 			</div>
