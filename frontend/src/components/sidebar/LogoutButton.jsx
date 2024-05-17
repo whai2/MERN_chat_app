aabbd24987ff6cd1f258fba1d953ccd632fdb5fb
whai2 <whai2125@gmail.com>
2024-05-17 14:14:47 +0900

    feat: 커스텀 훅을 이용한 로그아웃 구현

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,18 +1,37 @@
 import { BiLogOut } from "react-icons/bi";
-// import useLogout from "../../hooks/useLogout";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-	// const { loading, logout } = useLogout();
+	const { loading, logout } = useLogout();
 
 	return (
 		<div className='mt-auto'>
-			{/* {!loading ? (
+			{!loading ? (
 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
 			) : (
 				<span className='loading loading-spinner'></span>
-			)} */}
-      <BiLogOut className='w-6 h-6 text-white cursor-pointer' />
+			)}
 		</div>
 	);
 };
 export default LogoutButton;
+
+// ui 기능만 구현
+// import { BiLogOut } from "react-icons/bi";
+// // import useLogout from "../../hooks/useLogout";
+
+// const LogoutButton = () => {
+// 	// const { loading, logout } = useLogout();
+
+// 	return (
+// 		<div className='mt-auto'>
+// 			{/* {!loading ? (
+// 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+// 			) : (
+// 				<span className='loading loading-spinner'></span>
+// 			)} */}
+//       <BiLogOut className='w-6 h-6 text-white cursor-pointer' />
+// 		</div>
+// 	);
+// };
+// export default LogoutButton;
